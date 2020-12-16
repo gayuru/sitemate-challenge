f0b94da5d9b057440f5fcd29e7817e3216ec418d
Gayuru <ggayurugunawardana@gmail.com>
2020-12-16 21:24:27 +1100

    Added css styling to buttons and title

--- src/test.js
+++ src/test.js
@@ -1,19 +1,17 @@
-var clickMeButton = document.getElementById('clickButton');
-
 var favDialog = document.getElementById('favDialog');
 var outputBox = document.querySelector('output');
 var modalTitle = document.getElementById('title');
 
-function showPopup(val){
+function showPopup(val) {
   if (typeof favDialog.showModal === "function") {
     favDialog.showModal();
-    title.innerHTML = val;
+    modalTitle.innerHTML = val;
   } else {
     alert("The <dialog> API is not supported by this browser");
   }
 }
 
-function buttonClick(val){
+function buttonClick(val) {
   outputBox.value = `You just clicked "${val}"`;
 }
 
