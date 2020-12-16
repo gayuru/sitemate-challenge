50d4c1615cbfcb891cdac9b2b3ba5bc24cc22860
Gayuru <ggayurugunawardana@gmail.com>
2020-12-16 21:11:42 +1100

    Removed unnecessary Imports

--- src/test.js
+++ src/test.js
@@ -2,8 +2,6 @@ var clickMeButton = document.getElementById('clickButton');
 
 var favDialog = document.getElementById('favDialog');
 var outputBox = document.querySelector('output');
-var confirmBtn = document.getElementById('confirmBtn');
-var cancelBtn = document.getElementById('cancelBtn');
 var modalTitle = document.getElementById('title');
 
 function showPopup(val){
@@ -18,17 +16,5 @@ function showPopup(val){
 function buttonClick(val){
   outputBox.value = `You just clicked "${val}"`;
 }
-// cancelBtn.addEventListener('click', function(){
-//     outputBox.value = `You just clicked "Cancel"`;
-// });
 
-// confirmBtn.addEventListener('click', function(){
-//     outputBox.value = `You just clicked "Yes"`;
-// });
-
-
-// // "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
-// favDialog.addEventListener('close', function onClose() {
-//   outputBox.value = "You just clicked Yes" ;
-// });
 
